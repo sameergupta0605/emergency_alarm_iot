d3e731dc0e40a37f556255fd47169b6800ce4715
sameergupta0605 <67657976+sameergupta0605@users.noreply.github.com>
2021-06-30 10:15:12 +0530

    Update sketch_jun20a.ino

--- sketch_jun20a.ino
+++ sketch_jun20a.ino
@@ -3,12 +3,12 @@
 #include <ESP8266WiFi.h>
 #include <ThingerESP8266.h>
 
-#define USERNAME "sameergupta"
-#define DEVICE_ID "nm0"
-#define DEVICE_CREDENTIAL "ZXfSckK+rD9oC&u!"
+#define USERNAME "username on thinger.io"
+#define DEVICE_ID "device id on thinger.io"
+#define DEVICE_CREDENTIAL "device credentials from thinger.io"
 
-#define SSID "Homenet"
-#define SSID_PASSWORD "homenet@123"
+#define SSID "your ssid"
+#define SSID_PASSWORD "your password"
 
 ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 int pushPin = 12; //  Pin 6 on Node MCU 
